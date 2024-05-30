c2386236b4c4d239ccb7bb1649702dc43ab3886c
Kyle Stanfield <kjstan@gmail.com>
2024-05-30 19:58:34 -0400

    Current Version

--- js/script.js
+++ js/script.js
@@ -1,33 +1,14 @@
 //On Load
-// window.onload = function () {
-//   const apiUrl =
-//     "https://n8n.kjlab.io/webhook/3ee224f4-35ef-4541-b264-6e4ff8d40c40";
+window.onload = function () {
+  reset();
+};
 
-//   fetch(apiUrl)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok " + response.statusText);
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log("Success:", data);
-//       current_status = data.in;
-//     })
-//     .catch((error) => {
-//       console.error("There was a problem with the fetch operation:", error);
-//     });
+//Check and report most recent status
+function reset() {
+  const apiUrl = "https://n8n.kjlab.io/webhook/clockin";
+  const params = new URLSearchParams({ type: "check" });
 
-//   // Update the text in the div
-//   document.getElementById("status").innerText = `Status ${status}`;
-// };
-
-//Button Press
-document.getElementById("clock-btn").addEventListener("click", function () {
-  const apiUrl =
-    "https://n8n.kjlab.io/webhook/3ee224f4-35ef-4541-b264-6e4ff8d40c40";
-
-  fetch(apiUrl)
+  fetch(`${apiUrl}?${params.toString()}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
@@ -36,8 +17,54 @@ document.getElementById("clock-btn").addEventListener("click", function () {
     })
     .then((data) => {
       console.log("Success:", data);
-      let current_status = data.in;
-      console.log(current_status);
+      let current_status = data.status;
+      let status_time = data.time_in;
+
+      console.log("status", current_status);
+
+      //Check current status and update div
+      if (current_status == "in") {
+        document.getElementById("status").innerText = "Status: In Use";
+        document.getElementById("time").innerText = `Since: ${status_time}`;
+        document.getElementById("clock-btn").innerText = `CLOCK OUT`;
+        document.getElementById("status").classList.remove("free");
+        document.getElementById("status").classList.add("inuse");
+      } else {
+        document.getElementById("status").innerText = "Status: Free";
+        document.getElementById("time").innerText = `Since: ${status_time}`;
+        document.getElementById("clock-btn").innerText = `CLOCK IN`;
+        document.getElementById("status").classList.remove("inuse");
+        document.getElementById("status").classList.add("free");
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+//Button Press
+document.getElementById("clock-btn").addEventListener("click", function () {
+  const apiUrl = "https://n8n.kjlab.io/webhook/clockin";
+  let action = "null";
+
+  if (document.getElementById("clock-btn").innerText == `CLOCK OUT`) {
+    action = "out";
+  } else {
+    action = "in";
+  }
+
+  let params = new URLSearchParams({ type: `${action}` });
+
+  fetch(`${apiUrl}?${params.toString()}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      reset();
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
