2b6a0374f05d7b5028fe23ac49c7246df50d8c9b
Kyle Stanfield <kjstan@gmail.com>
2024-05-30 18:53:27 -0400

    testing db

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
+let current_status = null;
+
+//On Load
+// window.onload = function () {
+//   const apiUrl =
+//     "https://n8n.kjlab.io/webhook/3ee224f4-35ef-4541-b264-6e4ff8d40c40";
+
+//   fetch(apiUrl)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok " + response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("Success:", data);
+//       current_status = data.in;
+//     })
+//     .catch((error) => {
+//       console.error("There was a problem with the fetch operation:", error);
+//     });
+
+//   // Update the text in the div
+//   document.getElementById("status").innerText = `Status ${status}`;
+// };
+
+//Button Press
 document.getElementById("clock-btn").addEventListener("click", function () {
   const apiUrl =
-    "https://n8n.kjlab.io/webhook/3ee224f4-35ef-4541-b264-6e4ff8d40c40"; // Replace with your API URL
+    "https://n8n.kjlab.io/webhook/3ee224f4-35ef-4541-b264-6e4ff8d40c40";
 
   fetch(apiUrl)
     .then((response) => {
@@ -11,6 +38,8 @@ document.getElementById("clock-btn").addEventListener("click", function () {
     })
     .then((data) => {
       console.log("Success:", data);
+      current_status = data.in;
+      console.log(current_status);
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
